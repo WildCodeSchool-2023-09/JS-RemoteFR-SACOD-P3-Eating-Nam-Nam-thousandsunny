b37d0b32829130d4ffbf4282111a736eac5c8100
Hugo Durand <git config --global user.email hugo.durand143@gmail.comgit config --global user.email>
2023-12-11 16:27:37 +0100

    NavBarV1

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./style/Navbar.scss";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <img src="/src/assets/logo-eating.png" alt="Logo" />
+    </div>
+  );
+}
+
+export default Navbar;
