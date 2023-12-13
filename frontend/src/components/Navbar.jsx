1c6003d2e1d24aa2651767ae8f2eb363cd08a010
Flavien G <44913995+fgaujard@users.noreply.github.com>
2023-12-13 09:21:06 +0100

    SearchbarAdd

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,41 @@
+import searchIcon from "../assets/searchIcon.svg";
+import namNamIcon from "../assets/namNamIcon.svg";
+import burgerMenuIcon from "../assets/burgerMenuIcon.svg";
+import goNamNam from "../assets/GO.svg";
 import "./style/Navbar.scss";
 
 function Navbar() {
   return (
-    <div className="Navbar">
-      <p>coucou</p>
-      <img src="/src/assets/logo-eating.png" alt="Logo" />
-      <p>coucou</p>
-    </div>
+    <>
+      <div className="Navbar">
+        <img src={searchIcon} alt="Rechercher" className="SearchIcon" />
+        <img
+          src={namNamIcon}
+          alt="Page d'Accueil"
+          className="HomeIcon"
+          href="/"
+        />
+        <div className="RightSide">
+          <div className="SearchBar SearchBarDesktop">
+            <input
+              type="search"
+              placeholder="Entrer votre recherche ici..."
+              className="SearchInput"
+            />
+            <img src={goNamNam} alt="Rechercher" className="GoIcon" />
+          </div>
+          <img src={burgerMenuIcon} alt="Menu" className="BurgerIcon" />
+        </div>
+      </div>
+      <div className="SearchBar SearchBarMobile">
+        <input
+          type="search"
+          placeholder="Entrer votre recherche ici..."
+          className="SearchInput"
+        />
+        <img src={goNamNam} alt="Rechercher" className="GoIcon" />
+      </div>
+    </>
   );
 }
 
