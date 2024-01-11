4c0ac26c4bce2ddb78a467a3247ae33d434f70ca
TRISTAN ZVUNKA <tristan.zvunka@gmail.com>
2024-01-11 10:10:52 +0100

    add values in DB and fixed syntax issues in SQL schema

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE recipe (
   ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     user_ID INT NOT NULL,
     name VARCHAR(80) NOT NULL,
-    titre VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
     prep_time INT NOT NULL,
     nb_people INT NOT NULL,
     difficulty VARCHAR(30) NOT NULL,
